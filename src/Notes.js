d9d7286b72e071a70d0fb8c80966cf9f0940c2e2
Shekharpawar1 <86783159+Shekharpawar1@users.noreply.github.com>
2021-11-27 12:46:35 +0530

    first commit in secondary

---
+++ src/Notes.js
@@ -0,0 +1,6 @@
+export default function Notes(){
+    return (<div className="notes">
+        <h1>this is the notes title</h1>
+        <p> this is the notes content</p>
+    </div>)
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-import React, {Fragment, StrictMode}  from "react";
+import React, { StrictMode}  from "react";
 import ReactDom from "react-dom";
 
 import "./style.css"
 
 //importing the module from the different js files
-//import App from "./App"
-import Header from "./Header"
-import Footer  from "./Footer";
+import App from "./App"
+
 
 const rootElement=document.getElementById("root");
-const element=(<React.Fragment>
-  <Header/>
-  <Footer/>
-</React.Fragment>)
 
-// ReactDom.render({return (
-//   <React.Fragment>
-//     <Header/>
-//     <Footer/>
-//   </React.Fragment>
-// );
-// },rootElement)
-
-ReactDom.render(element,rootElement)
- 
-
-
+ReactDom.render(<StrictMode><App/></StrictMode>,rootElement)
